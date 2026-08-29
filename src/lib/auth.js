35b859da44d8114cf00b7b7a8de62ea35abbe3c6
bungex <100457224+bungex@users.noreply.github.com>
2026-08-28 22:10:21 -0400

    fix: run prisma generate before build on Netlify

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,6 +5,7 @@ import bcrypt from "bcryptjs";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   providers: [
